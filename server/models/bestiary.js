305737f894176b806a28df0b96d8e59abb822afc
Ryan Haswell <haswellr@hotmail.com>
2016-02-11 23:25:08 -0800

    added autopopulate module so bestiary will autopopulate with its creatures

--- server/models/bestiary.js
+++ server/models/bestiary.js
@@ -1,11 +1,14 @@
 var mongoose = require('mongoose');
+var autopopulate = require('mongoose-autopopulate');
 
 var BestiarySchema = new mongoose.Schema(
 	{
         creatures: [{
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Creature'
+            ref: 'Creature',
+            autopopulate: true
         }]
     });
+BestiarySchema.plugin(autopopulate);
 
 module.exports = mongoose.model('Bestiary',BestiarySchema);
