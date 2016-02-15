9c6dea768e877dc8bf4ad195118e01c8b8cb8e3f
haswellr <haswellr@hotmail.com>
2016-02-14 20:19:10 -0800

    lots of progress on create creature front end. still need to implement abilities, actions, and reactions.

--- server/public/js/controllers/creature.js
+++ server/public/js/controllers/creature.js
@@ -9,6 +9,10 @@ var creatureCtrl = function($scope,creature,Creature) {
 		races: ["Human","Dwarf","Elf","Halfling","Gnome","Dragonborn","Undead","Beast","Elemental","Ooze","Giant","Construct"],
 		alignments: ["Unaligned","Lawful Good","Lawful Neutral","Lawful Evil","Neutral Good","Neutral","Neutral Evil","Chaotic Good","Chaotic Neutral","Chaotic Evil"],
 		armorTypes: ["Natural Armor","Padded","Leather","Studded leather","Hide","Chain shirt","Scale mail","Breastplate","Half plate","Ring mail","Chain mail","Splint","Plate"],
+		abilities: ["strength","dexterity","constitution","intelligence","wisdom","charisma"],
+		skills: ["Acrobatics","Animal Handling","Arcana","Athletics","Deception","History","Insight","Intimidation","Investigation","Medicine","Nature","Perception","Performance","Persuasion","Religion","Sleight of Hand","Stealth","Survival"],
+		damageTypes: ["Slashing","Piercing","Bludgeoning","Acid","Fire","Cold","Poison","Necrotic","Radiant","Lightning","Psychic","Thunder","Force"],
+		languages: ["Common","Dwarvish","Elvish","Giant","Gnomish","Goblin","Halfling","Orc","Abyssal","Celestial","Draconic","Deep Speech","Infernal","Primordial","Sylvan","Undercommon"],
 		search: function(searchText,arrayToSearch){
 			var returnedVals = [];
 			if(searchText && arrayToSearch){
@@ -22,10 +26,110 @@ var creatureCtrl = function($scope,creature,Creature) {
 		}
 	};
 
+	$scope.savingThrows = {
+		isUsed: function(savingThrowAbility){
+			for(var i=0;i<$scope.creature.stats.savingThrows.length;i++){
+				if(savingThrowAbility.toLowerCase() == $scope.creature.stats.savingThrows[i].ability.toLowerCase()){
+					return true;
+				}
+			}
+			return false;
+		},
+		getUnused: function(selectedSavingThrow){
+			if(!$scope.creature.stats || !$scope.creature.stats.savingThrows)
+				return $scope.creatureData.abilities;
+			var unusedAbilities = [];
+			for(var i=0;i<$scope.creatureData.abilities.length;i++){
+				var unused = !$scope.savingThrows.isUsed($scope.creatureData.abilities[i]);
+				if(unused || selectedSavingThrow.ability.toLowerCase()==$scope.creatureData.abilities[i].toLowerCase())
+					unusedAbilities.push($scope.creatureData.abilities[i]);
+			}
+			return(unusedAbilities);
+		},
+		add: function(){
+			if(!$scope.creature.stats)
+				$scope.creature.stats = {};
+			if(!$scope.creature.stats.savingThrows)
+				$scope.creature.stats.savingThrows = [];
+			for(var i=0;i<$scope.creatureData.abilities.length;i++){
+				var unused = !$scope.savingThrows.isUsed($scope.creatureData.abilities[i]);
+				if(unused){
+					var newSavingThrow = {
+						ability: $scope.creatureData.abilities[i],
+						proficient: true
+					};
+					$scope.creature.stats.savingThrows.push(newSavingThrow);
+					break;
+				}
+			}
+		},
+		remove: function(savingThrow){
+			var index = $scope.creature.stats.savingThrows.indexOf(savingThrow);
+			if(index!=-1)
+				$scope.creature.stats.savingThrows.splice(index,1);
+		}
+	};
+
+	$scope.skills = {
+		isUsed: function(skillName){
+			for(var i=0;i<$scope.creature.stats.skills.length;i++){
+				if(skillName.toLowerCase() == $scope.creature.stats.skills[i].name.toLowerCase()){
+					return true;
+				}
+			}
+			return false;
+		},
+		getUnused: function(selectedSkill){
+			if(!$scope.creature.stats || !$scope.creature.stats.skills)
+				return $scope.creatureData.skills;
+			var unusedSkills = [];
+			for(var i=0;i<$scope.creatureData.skills.length;i++){
+				var unused = !$scope.skills.isUsed($scope.creatureData.skills[i]);
+				if(unused || selectedSkill.name.toLowerCase()==$scope.creatureData.skills[i].toLowerCase())
+					unusedSkills.push($scope.creatureData.skills[i]);
+			}
+			return(unusedSkills);
+		},
+		add: function(){
+			if(!$scope.creature.stats)
+				$scope.creature.stats = {};
+			if(!$scope.creature.stats.skills)
+				$scope.creature.stats.skills = [];
+			for(var i=0;i<$scope.creatureData.skills.length;i++){
+				var unused = !$scope.skills.isUsed($scope.creatureData.skills[i]);
+				if(unused){
+					var newSkill = {
+						name: $scope.creatureData.skills[i],
+						proficient: true
+					};
+					$scope.creature.stats.skills.push(newSkill);
+					break;
+				}
+			}
+		},
+		remove: function(skill){
+			var index = $scope.creature.stats.skills.indexOf(skill);
+			if(index!=-1)
+				$scope.creature.stats.skills.splice(index,1);
+		}
+	};
+
 
 	$scope.$watch("creature",function(newValue,oldValue){
 		Creature.calculateCreatureDetails($scope.creature);
 	},true);
+}
+
+var defaultCreature = {
+	flavor: {},
+	stats: {
+		damageVulnerabilities: [],
+		damageResistances: [],
+		damageImmunities: [],
+		conditionImmunities: [],
+		senses: [],
+		languages: []
+	}
 }
 
 //don't load controller until we've gotten the data from the server
@@ -41,7 +145,7 @@ creatureCtrl.resolve = {
 					return deferred.promise;
 				}
 				else
-					return {};
+					return defaultCreature;
 			}
 		}
 
