aeb6628844e5291ab1408576756aae19212b3ef2
haswellr <haswellr@hotmail.com>
2016-02-14 17:44:06 -0800

    working on create/edit front end. unfinished.

--- server/public/js/services/creature.js
+++ server/public/js/services/creature.js
@@ -54,69 +54,87 @@ angular.module('myApp').factory("Creature", function($resource,$sce) {
 
 	serv.calculateCreatureDetails = function(creature){
 		//displayed armor type
-		if(creature.stats.armorType=="")
-			creature.stats.armorTypeStr = "";
-		else
-			creature.stats.armorTypeStr = "("+creature.stats.armorType+")";
+		if(creature.stats && creature.stats.armorType){
+			if(creature.stats.armorType=="")
+				creature.stats.armorTypeStr = "";
+			else
+				creature.stats.armorTypeStr = "("+creature.stats.armorType+")";
+		}
 		//ability modifiers
-		creature.stats.abilityScoreModifiers = {};
-		creature.stats.abilityScoreStrs = {};
-		for(var key in creature.stats.abilityScores){
-			if(creature.stats.abilityScores.hasOwnProperty(key)){
-				creature.stats.abilityScoreModifiers[key] = getAbilityModifier(creature.stats.abilityScores[key]);
-				var sign = "+";
-				if(creature.stats.abilityScoreModifiers[key]<0)
-					sign = "–";
-				creature.stats.abilityScoreStrs[key] = creature.stats.abilityScores[key]+" ("+sign+Math.abs(creature.stats.abilityScoreModifiers[key])+")";
+		if(creature.stats && creature.stats.abilityScores){
+			creature.stats.abilityScoreModifiers = {};
+			creature.stats.abilityScoreStrs = {};
+			for(var key in creature.stats.abilityScores){
+				if(creature.stats.abilityScores.hasOwnProperty(key)){
+					creature.stats.abilityScoreModifiers[key] = getAbilityModifier(creature.stats.abilityScores[key]);
+					var sign = "+";
+					if(creature.stats.abilityScoreModifiers[key]<0)
+						sign = "–";
+					creature.stats.abilityScoreStrs[key] = creature.stats.abilityScores[key]+" ("+sign+Math.abs(creature.stats.abilityScoreModifiers[key])+")";
+				}
 			}
 		}
 		//hit points
-		creature.stats.hitDieSize = hitDieSize[creature.stats.size];
-		creature.stats.extraHealthFromConstitution = creature.stats.abilityScoreModifiers["constitution"] * creature.stats.numHitDie;
-		creature.stats.hitPoints = Math.floor(creature.stats.numHitDie * ((creature.stats.hitDieSize/2.0) + 0.5 + creature.stats.abilityScoreModifiers["constitution"]));
+		if(creature.stats && creature.stats.size && creature.stats.abilityScoreModifiers && creature.stats.numHitDie){
+			creature.stats.hitDieSize = hitDieSize[creature.stats.size];
+			creature.stats.extraHealthFromConstitution = creature.stats.abilityScoreModifiers["constitution"] * creature.stats.numHitDie;
+			creature.stats.hitPoints = Math.floor(creature.stats.numHitDie * ((creature.stats.hitDieSize/2.0) + 0.5 + creature.stats.abilityScoreModifiers["constitution"]));
+		}
 		//make ability descriptions html safe so we can use italics and other markup
-		for(var index in creature.stats.additionalAbilities){
-			var ability = creature.stats.additionalAbilities[index];
-			ability.descriptionHtml = $sce.trustAsHtml(ability.description);
+		if(creature.stats && creature.stats.additionalAbilities){
+			for(var index in creature.stats.additionalAbilities){
+				var ability = creature.stats.additionalAbilities[index];
+				ability.descriptionHtml = $sce.trustAsHtml(ability.description);
+			}
 		}
-		for(var index in creature.stats.actions){
-			var ability = creature.stats.actions[index];
-			ability.descriptionHtml = $sce.trustAsHtml(ability.description);
+		if(creature.stats && creature.stats.actions){
+			for(var index in creature.stats.actions){
+				var ability = creature.stats.actions[index];
+				ability.descriptionHtml = $sce.trustAsHtml(ability.description);
+			}
 		}
-		for(var index in creature.stats.reactions){
-			var ability = creature.stats.reactions[index];
-			ability.descriptionHtml = $sce.trustAsHtml(ability.description);
+		if(creature.stats && creature.stats.reactions){
+			for(var index in creature.stats.reactions){
+				var ability = creature.stats.reactions[index];
+				ability.descriptionHtml = $sce.trustAsHtml(ability.description);
+			}
 		}
 		//make description html safe so we can use italics and other markup
-		creature.flavor.descriptionHtml = $sce.trustAsHtml(creature.flavor.description);
+		if(creature.flavor && creature.flavor.description!=undefined){
+			creature.flavor.descriptionHtml = $sce.trustAsHtml(creature.flavor.description);
+		}
 		//saving throws
-		for(var index in creature.stats.savingThrows){
-			var savingThrow = creature.stats.savingThrows[index];
-			var mod = creature.stats.abilityScoreModifiers[savingThrow.ability];
-			if(savingThrow.value)
-				mod = savingThrow.value;
-			else if(savingThrow.proficient)
-				mod = mod + creature.stats.proficiencyBonus;
-			savingThrow.modifier = mod;
-			var sign = "+";
-				if(mod<0)
-					sign = "–";
-			savingThrow.modifierStr = shortFormAbilities[savingThrow.ability]+" "+sign+Math.abs(mod);
+		if(creature.stats && creature.stats.savingThrows && creature.stats.abilityScoreModifiers && creature.stats.proficiencyBonus){
+			for(var index in creature.stats.savingThrows){
+				var savingThrow = creature.stats.savingThrows[index];
+				var mod = creature.stats.abilityScoreModifiers[savingThrow.ability];
+				if(savingThrow.value)
+					mod = savingThrow.value;
+				else if(savingThrow.proficient)
+					mod = mod + creature.stats.proficiencyBonus;
+				savingThrow.modifier = mod;
+				var sign = "+";
+					if(mod<0)
+						sign = "–";
+				savingThrow.modifierStr = shortFormAbilities[savingThrow.ability]+" "+sign+Math.abs(mod);
+			}
 		}
 		//skills
-		for(var index in creature.stats.skills){
-			var skill = creature.stats.skills[index];
-			var ability = skillAbilities[skill.name];
-			var mod = creature.stats.abilityScoreModifiers[ability];
-			if(skill.value)
-				mod = skill.value;
-			else if(skill.proficient)
-				mod = mod + creature.stats.proficiencyBonus;
-			skill.modifier = mod;
-			var sign = "+";
-				if(mod<0)
-					sign = "–";
-			skill.modifierStr = skill.name+" "+sign+Math.abs(mod);
+		if(creature.stats && creature.stats.skills && creature.stats.abilityScoreModifiers && creature.stats.proficiencyBonus){
+			for(var index in creature.stats.skills){
+				var skill = creature.stats.skills[index];
+				var ability = skillAbilities[skill.name];
+				var mod = creature.stats.abilityScoreModifiers[ability];
+				if(skill.value)
+					mod = skill.value;
+				else if(skill.proficient)
+					mod = mod + creature.stats.proficiencyBonus;
+				skill.modifier = mod;
+				var sign = "+";
+					if(mod<0)
+						sign = "–";
+				skill.modifierStr = skill.name+" "+sign+Math.abs(mod);
+			}
 		}
 	}
 
