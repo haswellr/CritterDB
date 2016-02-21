b7dc041b194bdbbde50748389c1c501292ce7f60
Ryan Haswell <haswellr@hotmail.com>
2016-02-20 23:01:30 -0800

    added user model to database along with api routes for users. implemented bcryptjs to hash passwords. does not check for authorization.

---
+++ server/models/user.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcryptjs'),
+    SALT_WORK_FACTOR = 10;
+
+var UserSchema = new Schema({
+    username: {
+      type: String,
+      required: true,
+      index: { 
+        unique: true 
+      } 
+    },
+    email: {
+      type: String,
+      required: true,
+      index: {
+        unique: true
+      }
+    },
+    password: {
+      type: String,
+      required: true
+    }
+});
+
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+      console.log("gen salt err: "+err);
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+          console.log("hash password err: "+err);
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('User', UserSchema);
