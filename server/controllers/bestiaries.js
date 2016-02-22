921c4257eeba228d8884aabdb8aec33407c8a01a
haswellr <haswellr@hotmail.com>
2016-02-22 00:43:41 -0800

    added token based authentication to bestiary routes that use a bestiary id

--- server/controllers/bestiaries.js
+++ server/controllers/bestiaries.js
@@ -1,6 +1,40 @@
 
 //Get mongoose model
 var Bestiary = require('../models/bestiary');
+var jwt = require("jsonwebtoken");
+var config = require("../config");
+
+exports.authenticateByOwner = function(req, res, next) {
+    var id = req.params.id;
+    var query = {'_id':id};
+
+    Bestiary.findOne(query, function (err, doc) {
+        if(err) {
+            res.status(400).send(err);
+        }
+        else if(doc){
+            var token = req.body.token || req.query.token || req.headers['x-access-token'];
+            if(token){
+                jwt.verify(token,config.secret,function(err,decoded){
+                    if(err)
+                        res.status(400).send("Failed to authenticate token.");
+                    else{
+                        if(decoded._doc._id != doc.owner._id)
+                            res.status(400).send("Not authorized for access.");
+                        else
+                            next();
+                    }
+                });
+            }
+            else{
+                res.status(403).send("No token provided.");
+            }
+        }
+        else{
+            res.status(400).send("Bestiary not found");
+        }
+    });
+};
 
 exports.findById = function(req, res) {
     var id = req.params.id;
