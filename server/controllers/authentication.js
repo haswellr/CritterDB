20841ae88fdf8c89ba9d2ad91b4b1a292b363dfd
haswellr <haswellr@hotmail.com>
2016-02-23 12:02:42 -0800

    added rememberme functionality to back end via persistent session tokens stored in cookies

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,7 +1,38 @@
 var jwt = require("jsonwebtoken");
 var config = require("../config");
 var User = require('../models/user');
+var PersistentSession = require('../models/persistentSession');
 
+var generateNewPersistentSession = function(user,res,callback){
+  var query = {'username':user.username};
+  PersistentSession.findOneAndRemove(query, function(err, doc, result){
+      if(err)
+        callback(err.errmsg);
+      else{
+        persistentSession = new PersistentSession({'username':user.username});
+        persistentSession.save(function (err, doc) {
+            if(err)
+                callback(err);
+            else {
+                res.cookie('bestiarymanagertoken',persistentSession.token);
+                res.cookie('bestiarymanagerusername',user.username);
+                callback(null,doc);
+            }
+        });
+      }
+  });
+}
+
+//takes:
+//{
+//  username (string)
+//  password (string - not hashed)
+//  rememberme (boolean)
+//}
+//alternately instead of password, can accept cookies with persistent session info:
+//cookies.bestiarymanagertoken (hashed token set by server, front end doesn't have to touch)
+//cookies.bestiarymanagerusername (username identifier set by server, front end doesn't have to touch)
+//These cookies will be set after a successful authentication with 'rememberme' set to true.
 exports.authenticate = function(req, res){
 	var query = {};
 	if(req.body.email)
@@ -17,8 +48,31 @@ exports.authenticate = function(req, res){
     	res.status(400).send("Authentication failed. User not found.");
     }
     else if (user) {
-      if(!req.body.password)
-        res.status(400).send("Authentication failed. Password required.");
+      if(!req.body.password){
+        if(!req.cookies.bestiarymanagertoken)
+          res.status(400).send("Authentication failed. Password required.");
+        else{
+          var persistentToken = req.cookies.bestiarymanagertoken;
+          PersistentSession.findOne({'username':user.username}, function(err,persistentSession){
+            if(err)
+              res.status(400).send("Authentication failed.");
+            else{
+              persistentSession.compareToken(persistentToken,function(err,isMatch){
+                generateNewPersistentSession(user,res,function(err,doc){
+                  if(err)
+                    res.status(400).send("Authentication failed. Unable to generate persistent session.");
+                  else{
+                    var token = jwt.sign(user, config.secret, {
+                      expiresIn: config.tokens.duration
+                    });
+                    res.send(token);
+                  }
+                });
+              });
+            }
+          });
+        }
+      }
       else{
         // check if password matches
         user.comparePassword(req.body.password,function(err,isMatch){
@@ -31,8 +85,17 @@ exports.authenticate = function(req, res){
               expiresIn: config.tokens.duration
             });
 
-            // return the information including token as JSON
-            res.send(token);
+            if(req.body.rememberme){
+              generateNewPersistentSession(user,res,function(err,doc){
+                if(err)
+                  res.status(400).send("Authentication failed. Unable to generate persistent session.");
+                else{
+                  res.send(token);
+                }
+              });
+            }
+            else
+              res.send(token);
           }
           else{
             res.status(403).send("Authentication failed. Incorrect password.");
@@ -41,6 +104,12 @@ exports.authenticate = function(req, res){
       }
     }
   });
+}
+
+exports.revokeAuthentication = function(req, res) {
+  res.clearCookie('bestiarymanagertoken');
+  res.clearCookie('bestiarymanagerusername');
+  res.send();
 }
 
 exports.getCurrentUser = function(req, res) {
