07545f4c3d777ca1ea89653af27cabdd45014578
haswellr <haswellr@hotmail.com>
2016-03-18 15:57:35 -0700

    improved the mdchips hack to allow for clicking on an autocompleted item

--- server/public/js/directives/mdChipOnBlur.js
+++ server/public/js/directives/mdChipOnBlur.js
@@ -1,19 +1,31 @@
-angular.module('myApp').directive('mdChips', function () {
+angular.module('myApp').directive('mdChips', function ($timeout) {
   return {
     restrict: 'E',
     require: 'mdChips', // Extends the original mdChips directive
     link: function (scope, element, attributes, mdChipsCtrl) {
-      mdChipsCtrl.onInputBlur = function () {
+
+      var mouseUpActions = [];
+
+      mdChipsCtrl.onInputBlur = function (event) {
         this.inputHasFocus = false;
 
-      // ADDED CODE
-        var chipBuffer = this.getChipBuffer();
-        if (chipBuffer != "") { // REQUIRED, OTHERWISE YOU'D GET A BLANK CHIP
+        mouseUpActions.push((function(){
+          var chipBuffer = this.getChipBuffer();
+          if (chipBuffer != "") { // REQUIRED, OTHERWISE YOU'D GET A BLANK CHIP
             this.appendChip(chipBuffer);
             this.resetChipBuffer();
-        }
-      // - EOF - ADDED CODE
+          }
+        }).bind(this));
       };
+
+      window.addEventListener('click', function(event){
+        while(mouseUpActions.length>0){
+          var action = mouseUpActions.splice(0,1)[0];
+          $timeout(function(){
+            $timeout(action);
+          });
+        }
+      },false);
     }
   }
 });
