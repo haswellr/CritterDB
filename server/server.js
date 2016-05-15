83c119207fc8c159faa2d0bab0be28ec2cb07264
Ryan Haswell <haswellr@hotmail.com>
2016-05-14 22:45:08 -0700

    added back end route to serve list of the current users favorite bestiaries

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ app.delete('/api/publishedbestiaries/:id', publishedBestiaries.deleteById);
 	//Filtered Lists
 	app.get('/api/publishedbestiaries/recent/:page', publishedBestiaries.findRecent);
 	app.get('/api/publishedbestiaries/popular/:page', publishedBestiaries.findPopular);
-	//app.get('/api/publishedbestiaries/favorites/:page', publishedBestiaries.findFavorites);
+	app.get('/api/publishedbestiaries/favorites/:page', publishedBestiaries.findFavorites);
 	//app.get('/api/publishedbestiaries/owned/:page', publishedBestiaries.findOwned);
 //Users
 app.get('/api/users/:id/bestiaries', users.findBestiariesByOwner);
