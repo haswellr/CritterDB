7565398d8fd48b2db0a2a46c846441a424d87234
Ryan Haswell <haswellr@hotmail.com>
2016-06-05 22:44:09 -0700

    added disclaimer to publishing process ensuring that publishers are only publishing content that they own

--- server/public/js/controllers/publishBestiary.js
+++ server/public/js/controllers/publishBestiary.js
@@ -12,16 +12,26 @@ var publishBestiaryCtrl = function ($scope,$mdDialog,baseBestiary,publishedBesti
 		$location.url("/publishedbestiary/view/"+id);
 	}
 
-	$scope.publish = function(){
-		Creature.getAllForBestiary(baseBestiary._id,function(data){
-			$scope.publishedBestiary.creatures = data;
-			PublishedBestiary.create($scope.publishedBestiary,function(data){
-					goToPublishedBestiary(data._id);
-					$mdDialog.cancel();
-				},function(err){
-					console.log("error: "+err);
-				});
+	$scope.publish = function(ev){
+		var confirm = $mdDialog.confirm()
+			.title("Confirm Ownership")
+			.textContent("Please confirm that the published content is legally yours to share and is not copyrighted or otherwise legally protected in a way that would prevent its publishing on this site.")
+			.ariaLabel("Confirm Publish")
+			.targetEvent(ev)
+			.ok("Confirm")
+			.cancel("Cancel");
+		$mdDialog.show(confirm).then(function() {
+			Creature.getAllForBestiary(baseBestiary._id,function(data){
+				$scope.publishedBestiary.creatures = data;
+				PublishedBestiary.create($scope.publishedBestiary,function(data){
+						goToPublishedBestiary(data._id);
+						$mdDialog.cancel();
+					},function(err){
+						console.log("error: "+err);
+					});
+			});
 		});
+
 	}
 
 	//updates, closes dialog, and gives the data to the resolved promise
