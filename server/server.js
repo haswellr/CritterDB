bb9c44857c1b546c27b4d72ab08b51d854428cff
Ryan Haswell <haswellrd@gmail.com>
2020-12-06 12:33:46 -0800

    Increasing max body size for very large bestiaries.

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ var health = require('./controllers/health');
 var app = express();
 
 //app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded());
 app.use(cookieParser());
 //server public folder
