367d4de70ede222393af44537b6370e64086426b
Michato <noatomer11@gmail.com>
2021-12-02 12:52:30 +0200

    restored sampleconfig that was accidentaly deleted by my mistakes

---
+++ server/sampleconfig.js
@@ -0,0 +1,22 @@
+//the server folder needs to hold a file called 'config.js' that looks similar to this
+module.exports = {
+	'secret': "secret-key-for-app",
+	'db': {
+		'url': "mongodb://user:pw@host:port/database-name",
+		'options': {
+			'keepAlive': true,
+			'useNewUrlParser': true,
+			'useUnifiedTopology': true,
+			'useCreateIndex': true,
+			'useFindAndModify': false
+		}
+	},
+	'tokens': {
+		'duration': 86400
+	},
+	'email': {
+		'address': 'email-address-to-send-from',
+		'name': 'Critter DB',
+		'password': 'email-address-password'
+	}
+};
